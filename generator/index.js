c06a180a1315b0a41b34a5c97a6206cddff57a63
xujian <782135@qq.com>
2018-09-12 19:58:40 +0800

    fix typo

--- generator/index.js
+++ generator/index.js
@@ -6,23 +6,14 @@ module.exports = (api, options, rootOptions) => {
   
     api.render(
       {
-        "./src/plugins/http/index.js": "./templates/plugins/http/index.js"
+        "./src/plugins/notify/index.js": "./templates/plugins/notify/index.js"
       },
       options
     );
   
     const fs = require("fs");
-    const helpers = require('./helpers')(api)
   
     api.onCreateComplete(() => {
-      helpers.updateMain(src => {
-        let vueImportIndex = src.findIndex(line => line.match(/^import Vue/));
-        let axiosImportIndex = src.findIndex(line => line.match(/\/plugins\/api/));
-        if(axiosImportIndex < 0){
-          src.splice(++vueImportIndex, 0, "import './plugins/http'");
-        }      
-        return src;
-      });
     });
   };
   
